740d2389cd62b03f7428054bcc6b94f9c3ecd19e
David Jeske <davidj@gmail.com>
2020-05-02 08:43:47 -0700

    now loads a diffuse texture and draws some text on the screen

--- LoadObjectTest/LoadAsset.cs
+++ LoadObjectTest/LoadAsset.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,14 @@ using Stride.Rendering.Materials.ComputeColors;
 using Stride.Graphics;
 using Stride.Core.Mathematics;
 
+
+using Stride.UI.Panels;
+using Stride.UI.Controls;
+using Stride.UI;
+using Stride.Rendering.Fonts;
+using Stride.Graphics.Font;
+
+
 // NOTE: REMEMBER TO CREATE AN EMPTY ENTITY, and add this script as a component!
 
 
@@ -29,6 +38,32 @@ namespace LoadObjectTest
     {
         // Declared public member fields and properties will show in the game studio
 
+        public SpriteFont myFont2;
+
+        public void CreateUI() {
+        // var fontSystem = Services.GetService<FontSystem>();
+        // var myFont = fontSystem.NewDynamic(10,"Orkney Regular",FontStyle.Regular);
+             var startButton = new Button
+            {
+                Content = new TextBlock {Font = myFont2, Text = "Touch to Start", TextColor = Color.Black, 
+                    HorizontalAlignment = HorizontalAlignment.Center, VerticalAlignment = VerticalAlignment.Center},                
+                Padding = new Thickness(77, 30, 25, 30),
+                MinimumWidth = 250f,
+            };
+            var mainCanvas = new Canvas();
+            mainCanvas.Children.Add(startButton);
+
+            mainCanvas.BackgroundColor = Color.Red;
+            var mainMenuRoot = new ModalElement{
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                // VerticalAlignment = VerticalAlignment.Bottom,
+                DefaultHeight = 200,
+                Content = mainCanvas 
+                };
+            
+
+            Entity.Get<UIComponent>().Page = new UIPage { RootElement = mainMenuRoot };
+        }
 
         public void LoadAssetTest() {
         
@@ -56,8 +91,8 @@ namespace LoadObjectTest
             // https://github.com/stride3d/stride/blob/master/sources/editor/Stride.Assets.Presentation/AssetEditors/Gizmos/LightSpotGizmo.cs#L168
 
             var CWD = System.IO.Directory.GetCurrentDirectory();
-
-            var assetPath = System.IO.Path.GetFullPath(System.IO.Path.Combine(CWD,@"..\..\..\LoadObjectTest\DynLoadAssets\drone2\Drone2.obj"));
+            var assetBase = System.IO.Path.GetFullPath(System.IO.Path.Combine(CWD,@"..\..\..\LoadObjectTest\DynLoadAssets\drone2\"));
+            var assetPath = (System.IO.Path.Combine(assetBase,"Drone2.obj"));
 
             if (!System.IO.File.Exists(assetPath)) {     
                 // not sure why, but DebugText.Print isn't working at the time of testing this...
@@ -67,7 +102,7 @@ namespace LoadObjectTest
 
             // load the wavefront OBJ file...
             var wfData = new SimpleScene.Util3d.WavefrontObjLoader(assetPath);
-            
+                        
             // TODO: iterate over materials / multiple materials on the same mesh...
             {
                 VertexPositionNormalTexture[] vertices;                
@@ -87,23 +122,60 @@ namespace LoadObjectTest
                             VertexBuffers = new[] { new VertexBufferBinding(vertexBuffer, VertexPositionNormalTexture.Layout, vertexBuffer.ElementCount) },               
                             IndexBuffer = new IndexBufferBinding(indexBuffer, true, triIndices.Length),                            
                         } };            
-                // add the mesh to the model
+
+                // set the material index for this mesh
+                // customMesh.MaterialIndex = 0;
+
+                // add the mesh to the model                
                 model.Meshes.Add(customMesh);
             }
 
+            /*
+            this is some sample code of how xenko reads a stream
 
+            Texture texture;
+            using (var inStream = game.Content.OpenAsStream(filePath, StreamFlags.None))
+                texture = Texture.Load(device, inStream);
+        
+            var tempStream = new MemoryStream();
+            texture.Save(game.GraphicsContext.CommandList, tempStream, intermediateFormat);
+            tempStream.Position = 0;
+            texture.Dispose();
+
+            using (var inStream = game.Content.OpenAsStream(filePath, StreamFlags.None))
+            using (var originalImage = Image.Load(inStream))
+            {
+                using (var textureImage = Image.Load(tempStream))
+                {
+                    TestImage.CompareImage(originalImage, textureImage, false, 0, fileName);
+                }
+            }        
+             */
+
+
+            // load a texture from a file
+            
+            var diffuseTextureFilename = System.IO.Path.Combine(assetBase,wfData.materials[0].mtl.diffuseTextureResourceName);
+            var diffTexStream = System.IO.File.Open(diffuseTextureFilename,System.IO.FileMode.Open,System.IO.FileAccess.Read);
+            var diffuseTexture = Texture.Load(GraphicsDevice,diffTexStream);
+            
+
+            var cc = new ComputeColor();
+            
             // Create a material (eg with red diffuse color).
             var materialDescription = new Stride.Rendering.Materials.MaterialDescriptor
             {
                 Attributes =
-                {
+                {                   
                     DiffuseModel = new MaterialDiffuseLambertModelFeature(),
-                    Diffuse = new MaterialDiffuseMapFeature(new ComputeColor { Key = MaterialKeys.DiffuseValue })
+                    // Diffuse = new MaterialDiffuseMapFeature(new ComputeColor { Key = MaterialKeys.DiffuseValue }),
+                    Diffuse = new MaterialDiffuseMapFeature(new ComputeTextureColor(diffuseTexture)),
                 }
             };
             var material = Material.New(GraphicsDevice, materialDescription);
-            material.Passes[0].Parameters.Set(MaterialKeys.DiffuseValue, Color.Red);
+            // material.Passes[0].Parameters.Set(MaterialKeys.DiffuseValue, Color.Red);            
             model.Materials.Add(material);
+            
 
            SceneSystem.SceneInstance.RootScene.Entities.Add(entity);
 
@@ -114,6 +186,7 @@ namespace LoadObjectTest
         {
             // Initialization of the script.
             LoadAssetTest();
+            CreateUI();
              
             //Find camera, I've only got one so this works. If you've got more 
             //than one, I guess you'll need to name them or something...
